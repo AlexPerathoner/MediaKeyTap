bd1402a89a0f0caa9b0a6cd0cc9b76fe55bf3f4e
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-12 01:22:53 +0100

    Revert "remove keys"
    This reverts commit 7489f565c8cab341c906d4ba416c33a5a6f91c7c.

--- MediaKeyTap/MediaKeyTap.swift
+++ MediaKeyTap/MediaKeyTap.swift
@@ -9,6 +9,11 @@
 import Cocoa
 
 public enum MediaKey {
+    case playPause
+    case previous
+    case next
+    case rewind
+    case fastForward
     case brightnessUp
     case brightnessDown
     case volumeUp
